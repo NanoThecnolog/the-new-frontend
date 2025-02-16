e9645e6a894f2e1a59e523a2b96189f477055612
NanoThecnolog <contato@ericssongomes.com>
2025-02-16 18:24:39 -0300

    Feat(context): Create context files

---
+++ src/contexts/newContext.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { ContextProps, ContextProviderProps } from "@/types/Context";
+import { UserData, UserProps } from "@/types/User";
+import { api } from "@/utils/api";
+import { debug } from "@/utils/debugFunction";
+import { toastLoading } from "@/utils/toastLoading";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useState } from "react";
+import { toast } from "react-toastify";
+
+export const NewContext = createContext({} as ContextProps)
+
+export function NewProvider({ children }: ContextProviderProps) {
+    const [user, setUser] = useState<UserProps>()
+    const router = useRouter()
+
+
+
+    async function signIn(email: string) {
+        try {
+            //setUser([])
+            const response = await api.get('/user/detail', {
+                params: {
+                    email
+                }
+            })
+            const data: UserData[] = response.data.data
+            const obj: UserProps = {
+                email: data[0].email,
+                newsLetters: data ? data.map(news => news.req_id) : []
+            }
+
+            setUser(obj)
+            debug("ligando o toast")
+            document.body.style.cursor = 'wait'
+            await toastLoading()
+            debug('parte pra redirecionar...')
+            document.body.style.cursor = 'default'
+            router.push('/dashboard/user')
+        } catch (err) {
+            console.log(err)
+            toast.error("Email não encontrado, confirme seu email e tente novamente!")
+        }
+    }
+
+    return (
+        <NewContext.Provider value={{ user, setUser, signIn }}>
+            {children}
+        </NewContext.Provider>
+    )
+}
+
+export function useTheNewContext() {
+    return useContext(NewContext)
+}
